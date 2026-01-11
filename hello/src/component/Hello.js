cc40f0d643cd0289a6cb8bf3fd6a7662d75da998
Gajanika <premalathaganeshalingam@gmail.com>
2026-01-12 00:35:07 +0530

    reate Hello functional component with proper export

---
+++ hello/src/component/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react'  //React library-ஐ import பண்ணுறோம்
+function Hello(){   //Functional Component  //First letter Capital → React rule
+    return ( //UI (JSX) return பண்ணும்   
+    <h1>Hello G</h1>);
+}
+export default Hello;//இந்த component-ஐ other files-ல் use பண்ண export
