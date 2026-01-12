617544f05f69a2168b9ccfcc88beb17f2650a961
Gajanika <premalathaganeshalingam@gmail.com>
2026-01-13 01:34:15 +0530

    Demonstrate React.createElement without JSX

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+
 import Welcome from './components/Welcome.js'
+import Welcome2 from './components/Welcome2.js'
 function App() {
   return (
     <div className="App">
      <Welcome/>
+     <Welcome2/>
     </div>
   );
 }
---
+++ jsx/src/components/Welcome2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Welcome2 =()=>{
+    return React.createElement(
+        'div',
+       {id:'hello',className:'dummyClass'},
+       React.createElement('h1', null, 'hellog')
+    );
+}
+export default Welcome2;
