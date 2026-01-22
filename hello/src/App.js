5fba9a1120e4c4e586c9956105c325f7e34563cc
Gajanika <premalathaganeshalingam@gmail.com>
2026-01-22 13:55:51 +0530

    ErrorBoundary

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
 import Hello from './component/Hello';
+import Hero from './component/Hero';
+import ErrorBoundary from './component/ErrorBoundary';
 function App() {
   return (
     <div className="App">
-      <Hello/>  
+    <Hello/>  
+       <ErrorBoundary>
+       <Hero heroName="Gaju"/>
+       </ErrorBoundary>
+       <ErrorBoundary>
+       <Hero heroName="Jathu"/>
+       </ErrorBoundary>
+       <ErrorBoundary>
+       <Hero heroName="GaJa"/>
+       </ErrorBoundary>
+       <ErrorBoundary>
+       <Hero heroName="Joker"/>
+       
+       </ErrorBoundary>
+          
       </div>
   );
 }
